b57ad138a8f63a3a2b7494752b530651f597aec6
Ilimitablerocky <guzmannahuel948@gmail.com>
2026-05-31 17:03:36 -0300

    Subida original del TP

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <mutex>
+#include <
+//int cantidad_Maxima_paquete 1550; 
+//int cantidad_maxixa_estanteria 100;
 
 using namespace std;
+
+/*int generar_prioridad_aleatoria{
+	return 1/0
+}
+///PARTE 1
+*/paquete Buscar_mayor_prioridad(){
+	for int f hasta cantidad_maxima_estanteria
+	se aplica aging
+	retorna paquete mayor prioridad
+}
+
+/*
+///PARTE 2
+//void productor(int cantidad_maxima){
+	for int f hasta cantidad maxima
+	paquete nuevo_paquete;
+	crear_paquete(nuevo_paquete, f, generar_prioridad_aleatoria);
+	ingresamos a la estanteria(array capacidadx max 100);
+	esperamos 90ms
+	guardamos el paquete;
+}
+*/
+
+///PARTE 3
+/*
+//void consumidor(int capacidad_maxima){
+	for hasta cantidad_maxima
+	esperamos la cinta(cola) que pasen 550 ms
+	saca el paquete
+	avisa lugar disponible cinta;
+
+	"paquete procesado"
+}
+	
+*/
+
+
+///PARTE 4
+/*
+void gestor(trabaja con el array c}y con la cola de datos{
+	busca en la estanteria(array) si hay disponibilidad wait
+	funcion Buscar_mayor_prioridad retorna paquete
+	avisa hya espacion disponible al array
+	espera 420 ms
+	coloca la caja en la cinta si hay espacio disponible
+	avisa se coloco caja
+}
+*/
+
+*/
 
 int main()
 {
