df3d836a1057650327c507f610b394c09ee62883
Daro <157760735+Daro0x@users.noreply.github.com>
2026-06-02 17:58:38 -0300

    Create gestor.h
    Se crea el gestor.h y espero a la librería que contenga la función buscar_mayor_prioridad()

---
+++ gestor.h
@@ -0,0 +1,11 @@
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include "paquete.h"
+#include "semaforo.h"
+//#include ".h" //HAY QUE INCLUIR UNA LIBRERIA QUE TENGA EL BUSCAR_MAYOR_PRIORIDAD()
+
+using namespace std;
+
+void gestor();
