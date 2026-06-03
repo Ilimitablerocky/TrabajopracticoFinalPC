4873ea88efbc7d07972b24c0208f61f3cd8d59ab
Nahuel Guzman <guzmannahuel948@gmail.com>
2026-06-03 15:33:54 -0300

    Update paquete.h
    actualizacion paquete.h se agrego chrono

--- paquete.h
+++ paquete.h
@@ -1,11 +1,15 @@
 #ifndef PAQUETE_H_INCLUDED
 #define PAQUETE_H_INCLUDED
+#include <iostream>
+#include <chrono>
 
 struct Paquete{
     int id;
     int prioridad;
-
+    std::chrono::system_clock::time_point creacion;
+    std::chrono::system_clock::time_point ingreso_cinta;
 };
-void Crear_Paquete(Paquete& p, int nro, int prioridad);
 
+void Crear_Paquete(Paquete& p, int nro, int prioridad);
+void mostrar_Paquete(Paquete& p);
 #endif // PAQUETE_H_INCLUDED
