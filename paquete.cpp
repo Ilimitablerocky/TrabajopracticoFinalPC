4b5be74e1fe068b87766b0ca70bf8ffca007c21b
Nahuel Guzman <guzmannahuel948@gmail.com>
2026-06-03 15:34:52 -0300

    Update paquete.cpp
    actualizacion paquete.cpp chrono

--- paquete.cpp
+++ paquete.cpp
@@ -5,4 +5,8 @@ void Crear_Paquete(Paquete& p, int nro, int prioridad){
     p.prioridad = prioridad;
 }
 
-
+void mostrar_Paquete(Paquete& p){
+    std::cout << "Paquete: " << p.id << std::endl;
+    std::cout << "Prioridad: " << p.prioridad << std::endl;
+    std::cout << "Fecha Creacion: " << "FECHA_DE_CREACION" << std::endl;
+}
