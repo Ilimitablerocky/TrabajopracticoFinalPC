018659c270d0a7cf1866c61cdc0793f66379de8d
Dario Souza Valente <157760735+Daro0x@users.noreply.github.com>
2026-06-05 16:46:33 -0300

    Update gestor.cpp
    Se acompaña a los cambios realizados en el .h

--- gestor.cpp
+++ gestor.cpp
@@ -1,6 +1,10 @@
 #include "gestor.h"
 
-void gestor() {  
+void gestor(int cantidad_Maxima_paquetes, int& cant_estanteria, Paquete estanteria[], 
+            queue<Paquete>& cinta, chrono::system_clock::time_point inicio_programa,
+            Semaforo& sem_estanteria, Semaforo& sem_espacio_estanteria,
+            Semaforo& sem_espacio_cinta, Semaforo& sem_cinta_llena,
+            Semaforo& mutex_estanteria, Semaforo& mutex_cinta) {  
     int procesados = 0;
 
     while (procesados < cantidad_Maxima_paquetes) {
