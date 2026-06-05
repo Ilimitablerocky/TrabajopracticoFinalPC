e4539b87cbe55c213e1bfa62ca87374103948565
Dario Souza Valente <157760735+Daro0x@users.noreply.github.com>
2026-06-05 16:45:09 -0300

    Update gestor.h
    Se agregan parametros para evitar la implementacion de variables locales

--- gestor.h
+++ gestor.h
@@ -7,4 +7,8 @@
 
 using namespace std;
 
-void gestor();
+void gestor(int cantidad_Maxima_paquetes, int& cant_estanteria, Paquete estanteria[], 
+            queue<Paquete>& cinta, chrono::system_clock::time_point inicio_programa,
+            Semaforo& sem_estanteria, Semaforo& sem_espacio_estanteria,
+            Semaforo& sem_espacio_cinta, Semaforo& sem_cinta_llena,
+            Semaforo& mutex_estanteria, Semaforo& mutex_cinta);
