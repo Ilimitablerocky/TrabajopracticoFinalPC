6b7b7e2b1d6cbeb963c06f0f873210dc27d36253
Nahuel Guzman <guzmannahuel948@gmail.com>
2026-06-05 22:08:50 -0300

    Update gestor.h
    agregar variables externas

--- gestor.h
+++ gestor.h
@@ -6,9 +6,26 @@
 #include "semaforo.h"
 
 using namespace std;
+extern const int cantidad_Maxima_Paquetes;
+extern const int cantidad_Maxima_Estanteria;
+extern const int cantidad_Maxima_cinta;
 
-void gestor(int cantidad_Maxima_paquetes, int& cant_estanteria, Paquete estanteria[], 
-            queue<Paquete>& cinta, chrono::system_clock::time_point inicio_programa,
-            Semaforo& sem_estanteria, Semaforo& sem_espacio_estanteria,
-            Semaforo& sem_espacio_cinta, Semaforo& sem_cinta_llena,
-            Semaforo& mutex_estanteria, Semaforo& mutex_cinta);
+//VARIABLE productor/gestor
+extern int cantidad_actual_estanteria;
+extern chrono::system_clock::time_point inicio_programa;
+
+//ESTANTERIA...
+extern Paquete estanteria[];
+extern std::mutex mutex_estanteria;
+extern Semaforo sem_estanteria;
+extern Semaforo sem_espacio_estanteria;
+
+//CINTA...
+extern std::queue<Paquete> cinta;
+extern std::mutex mutex_cinta;
+extern Semaforo sem_cinta;
+extern Semaforo sem_espacio_cinta;
+
+extern std::mutex mutex_consola;
+
+void gestor();
