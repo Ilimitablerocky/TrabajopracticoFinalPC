e88bc6731c8a41f12fc5789d68ed99b7c0c43d71
Bruno <bminosmym@gmail.com>
2026-06-06 03:02:47 -0300

    Update consumidor.h

--- consumidor.h
+++ consumidor.h
@@ -1,7 +1,6 @@
 #ifndef CONSUMIDOR_H_INCLUDED
 #define CONSUMIDOR_H_INCLUDED
-#include <iostream>
-#include <chrono>
+
 #include <queue>
 #include <thread>
 #include "semaforo.h"
@@ -12,6 +11,8 @@ extern std::queue<Paquete> cinta;
 extern std::mutex mutex_cinta;
 extern Semaforo sem_cinta;
 extern Semaforo sem_espacio_cinta;
+
+extern std::mutex mutex_consola;
 
 void consumidor(int id_consumidor, int paquetes_x_consumidores);
 
