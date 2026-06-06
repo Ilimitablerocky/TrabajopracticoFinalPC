4b3e722591c2d81bfb841e0a3c062251f8422dfe
Bruno <bminosmym@gmail.com>
2026-06-06 03:01:18 -0300

    Update productor.cpp

--- productor.cpp
+++ productor.cpp
@@ -16,29 +16,28 @@ void productor(int id, int paquetes_x_productor){
     for(int i=0; i < paquetes_x_productor; i++){
 
         Paquete nuevo;
-        int x = generar_prioridad_con_frecuencia(40);
-        
-        mutex_idpaquete.lock(); //protegemos el contador de paquetes
-        int id_temp = numero_paquete; // asignamos id_temp a una variable global la cual va incrementando
-        numero_paquete++; // se incrementa
-        mutex_idpaquete.unlock();
-
-        Crear_Paquete(nuevo, id_temp, x);
+        int x = generar_prioridad_con_frecuencia(8);
+        Crear_Paquete(nuevo,i+1,x);                                  //(producir paquete)
 
         wait(sem_espacio_estanteria);
 
+        std::this_thread::sleep_for(std::chrono::milliseconds(90)); //(colocar en estanteria)
+
         mutex_estanteria.lock();
-            estanteria[cantidad_actual_estanteria] = nuevo;
-            cantidad_actual_estanteria++;
+        estanteria[cantidad_actual_estanteria] = nuevo;
+        cantidad_actual_estanteria++;
+        if(nuevo.prioridad == 0){
+            cant_producidos_p0++;
+        }else{
+            cant_producidos_p1++;
+        }
         mutex_estanteria.unlock();
 
         signal(sem_estanteria);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(90));
-        std::cout<< "Productor: " << id << " Almaceno paquete en la estanteria prioridad: " << nuevo.prioridad <<std::endl;
+        mutex_consola.lock();
+        std::cout<< "Productor con ID: " << id << ", almaceno paquete ID: " << nuevo.id << " con prioridad: " << nuevo.prioridad << " en la estanteria." << std::endl;
+        mutex_consola.unlock();
     }
-
-
-
 
 };
